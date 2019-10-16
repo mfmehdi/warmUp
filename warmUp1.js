93e8284ffe141249009999a7d844e786fd6e24ba
hamam.alsaiity@gmail.com <medamineslaimia@gmail.com>
2019-10-16 10:35:30 +0200

    Mehdi farjallah

--- warmUp1.js
+++ warmUp1.js
@@ -1,4 +1,9 @@
 // 1-Create a third variable called z, assign x + y to it, and display it.
 var x = 5;
 var y = 10;
+var z=x+y;
+console.log(z);
 // 2-calculate the area of a square using varaiables.
+var l=5;
+var area=l*l;
+console.log(area)
