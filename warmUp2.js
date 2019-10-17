382579ecd7a60786ab040e3fb6e7ca333ada8d6d
hamam.alsaiity@gmail.com <medamineslaimia@gmail.com>
2019-10-17 13:43:58 +0200

    mehdiw

--- warmUp2.js
+++ warmUp2.js
@@ -2,3 +2,21 @@
 // 2-Write a function called sum that accepts two numbers as parameters, and sum them together but without suming them togther directly
 //you can only add one at each summetion, you'll need to use recursion in this.
 
+
+function greatest(a,b){
+		if(a===0) return b	
+			if(b===0) return a
+				if(a===b) return b
+				if(a<b) {
+			    var k=a
+			    a=b
+					b=k
+					
+				}	 
+return  greatest(a-b ,b)
+}
+
+function sum (a,b){
+   if(b===0) return a;
+	return sum(a+1,b-1)
+}
