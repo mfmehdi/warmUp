f0b6db42595032f2e4bf03206a500f3a2c0452da
mfmehdi <mfmehdi2@gmail.com>
2019-10-18 10:56:14 +0200

    18/10/2019 mehdi

--- warmUp3.js
+++ warmUp3.js
@@ -6,3 +6,24 @@
 // 2-write a function that takes an array as an argument and returns the element that is located in the middle of that array.
 
 // 3-Change all the numbers in the array to be multiplied by two for even indexes.
+
+
+var arrayFemales=[]
+var arrayMales=[]
+var arrayInstructors=[]
+
+function middle(array){
+
+	return array[Math.floor((array.length-1)/2)]
+}
+
+
+function multipliedByTwo(array){
+
+return  array.map((val)=>{
+ 	if(typeof(val)==="number"{
+ 	 return val *2;
+ 	}
+ 		return val
+ })
+}
